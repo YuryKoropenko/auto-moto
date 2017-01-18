56ea5c0e78709d0a7c00f56a91c7411d652143f1
Yury Koropenko <yurykoropenko@gmail.com>
2017-01-18 14:30:51 +0200

    commit 8

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+
+/**/
+	$('.basket').on('click', function() {
+		$(this).children('.basket__popup-wp').fadeIn();
+		$(this).children('.basket__popup').fadeIn();
+		$('.basket__popup-wp').on('click', function() {
+			$(this).fadeOut();
+			$('.basket__popup').fadeOut();
+			return false
+		});
+	});
 /**/
 	$('.menu__item').mouseenter(function() {
 		$(this).addClass('menu__link-active');
@@ -22,6 +33,21 @@ $(document).ready(function() {
 	$('.corz__b-center-2-row-lab').on('click', function() {
 		$(this).toggleClass('corz__b-center-2-row-lab-active');
 	});
+
+/**/
+
+	$(".filter__price-sl").slider({
+		range: true,
+		min: 0,
+		max: 8650,
+		values: [ 1435, 8650 ],
+		slide: function( event, ui ) {
+			$( ".filter__price-first" ).val( ui.values[ 0 ] );
+			$( ".filter__price-last" ).val( ui.values[ 1 ] );
+		}
+	});
+	$( ".filter__price-first" ).val( $( ".filter__price-sl" ).slider( "values", 0 ) );
+	$( ".filter__price-last" ).val( $( ".filter__price-sl" ).slider( "values", 1 ) );
 
 /**/
 	$('.contacts__tabs-wp').lightTabs();
